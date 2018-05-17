57f98f67b00d8deaf61d9fd81b2fee17c9f53c15
MelbexDeLeon <melbex11@gmail.com>
2018-05-17 21:50:09 +0800

    checker

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var cors= require("cors")
+var cors = require("cors");
 const addon = require("./build/Release/module");
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 app.get("/", function(req, res) {
   res.send("wowie");
@@ -27,6 +26,13 @@ app.get("/", function(req, res) {
 */
 app.post("/solve", function(req, res) {
   res.status(200).json(addon.solver(req.body.puzzle, req.body.type));
+  // addon.solver()
+});
+
+app.post("/check", function(req, res) {
+  res
+    .status(200)
+    .json(addon.checker(req.body.puzzle, req.body.to_check, req.body.type));
   // addon.solver()
 });
 
